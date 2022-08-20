b88d9bf0c51944b21d1dd5dec8b3a269e954f727
Artem <arnst.artyom@gmail.com>
2022-08-20 14:40:34 +0300

    web_commit

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -25,3 +25,42 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
 }
+
+function setImg(n) {
+    var i
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[n].style.display = "block";
+}
+
+function setMore() {
+    var text_style = document.getElementsByClassName('more_button')[0].textContent
+    let text = document.querySelector(".elm_p")
+    let text_div = document.querySelector('.elm_descript')
+    text.classList.toggle('more')
+    text_div.classList.toggle('more')
+    if (text_style == 'Подробнее') {
+        document.getElementsByClassName('more_button')[0].textContent='Скрыть';
+    }
+    else {
+        document.getElementsByClassName('more_button')[0].textContent='Подробнее';
+    }
+}
+
+function setMore_feedbacks() {
+    var text_style = document.getElementsByClassName('more_button_feed')[0].textContent
+    let text = document.querySelector(".feedback_text_p")
+    let text_div = document.querySelector(".feedbacks")
+    let last_div = document.querySelector(".last_feedback")
+    text_div.classList.toggle('more')
+    last_div.classList.toggle('more')
+    text.classList.toggle('more')
+    if (text_style == 'Подробнее') {
+        document.getElementsByClassName('more_button_feed')[0].textContent='Скрыть';
+    }
+    else {
+        document.getElementsByClassName('more_button_feed')[0].textContent='Подробнее';
+    }
+}
